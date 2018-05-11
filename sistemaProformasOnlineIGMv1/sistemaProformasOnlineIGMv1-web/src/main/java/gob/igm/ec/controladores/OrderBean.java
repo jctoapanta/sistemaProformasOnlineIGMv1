09d613aa96bf5be5bcffde98fdc64958f71a6423
PULE_DIEGO <PULE_DIEGO@TEC_DES16.igm.mil>
2018-05-11 12:46:58 -0500

    Aporte de Diego: creacion de servico OrderBean, titemServicio y la creacion de la pagina proformas

---
+++ sistemaProformasOnlineIGMv1/sistemaProformasOnlineIGMv1-web/src/main/java/gob/igm/ec/controladores/OrderBean.java
@@ -0,0 +1,248 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gob.igm.ec.controladores;
+
+/**
+ *
+ * @author PULE_DIEGO
+ */
+import gob.igm.ec.Tentidad;
+import gob.igm.ec.Titem;
+import gob.igm.ec.servicios.TitemServicio;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.ejb.EJB;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+ 
+@ManagedBean(name="order")
+@SessionScoped
+public class OrderBean implements Serializable{
+
+    /**
+     * @return the totalp
+     */
+    public BigDecimal getTotalp() {
+        return totalp;
+    }
+
+    /**
+     * @param totalp the totalp to set
+     */
+    public void setTotalp(BigDecimal totalp) {
+        this.totalp = totalp;
+    }
+
+	private static final long serialVersionUID = 1L;
+        private List<Titem> listaitems=new ArrayList<Titem>();
+        @EJB
+        private TitemServicio titemServicio;
+        private Titem item=new Titem();
+	String descripcion;
+		//String productName;
+		BigDecimal cantidad;
+                BigDecimal total;
+                BigInteger iva;
+                private int seleccionadoItem;
+                private Tentidad entidad=new Tentidad();
+                private BigDecimal totalp=BigDecimal.ZERO;
+           
+               
+               
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(BigDecimal cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public BigInteger getIva() {
+        return iva;
+    }
+
+    public void setIva(BigInteger iva) {
+        this.iva = iva;
+    }
+              
+	private static final ArrayList<Order> orderList = new ArrayList<Order>();
+
+	 
+	public ArrayList<Order> getOrderList() {
+ 
+		return orderList;
+ 
+	}
+        
+  
+	public String addAction() {
+            try{
+	        item=titemServicio.getDatos(this.getSeleccionadoItem());
+		Order order = new Order(item.getDescItem(), this.cantidad,item.getPvp(),item.getLIva());
+		orderList.add(order);
+                this.totalp = (this.cantidad.multiply(item.getPvp())).add(this.totalp);
+		
+		
+            }catch( Exception e ){}
+               return null;     
+	}
+ 
+	public String deleteAction(Order order) {
+	    
+		orderList.remove(order);
+                 this.totalp = (this.totalp).subtract( (this.cantidad.multiply(item.getPvp())));
+                         return null;
+	}
+
+    /**
+     * @return the listaitems
+     */
+    public List<SelectItem> getItems() {
+        listaitems=titemServicio.findAll();
+        List<SelectItem> principales = new ArrayList<SelectItem>();
+        for(Titem itemp:listaitems){
+            SelectItem selectItem=new SelectItem(itemp.getIdItem(),itemp.getDescItem()); 
+            principales.add(selectItem);
+        }
+        return principales;
+        //return listaitems;
+    }
+
+    /**
+     * @param listaitems the listaitems to set
+     */
+    public void setListaitems(List<Titem> listaitems) {
+        this.listaitems = listaitems;
+    }
+
+    /**
+     * @return the seleccionadoItem
+     */
+    public int getSeleccionadoItem() {
+        return seleccionadoItem;
+    }
+
+    /**
+     * @param seleccionadoItem the seleccionadoItem to set
+     */
+    public void setSeleccionadoItem(int seleccionadoItem) {
+        this.seleccionadoItem = seleccionadoItem;
+    }
+
+    /**
+     * @return the entidad
+     */
+    public Tentidad getEntidad() {
+
+         this.setCantidad(BigDecimal.ONE);
+        return entidad;
+    }
+
+    /**
+     * @param entidad the entidad to set
+     */
+    public void setEntidad(Tentidad entidad) {
+        this.entidad = entidad;
+    }
+
+    
+
+	public static class Order{
+		
+		String descripcion;
+                BigDecimal cantidad;
+                BigDecimal total;
+                BigInteger iva;
+                private Tentidad entidad=new Tentidad();
+                
+                public Order(){
+                     entidad.setCiu("1716542913");
+                     entidad.setNombres("Diego Oswaldo Pule López");
+                     entidad.setDireccion("Quito");
+                     
+                }
+                
+                
+                public Order(String descripcion, BigDecimal cantidad, BigDecimal total, BigInteger iva) {
+			this.descripcion = descripcion;
+			this.cantidad = cantidad;
+			this.total = total;
+			this.iva = iva;
+		}
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        public BigDecimal getCantidad() {
+            //detalleproforma.setCantidad(BigDecimal.ONE);
+            return cantidad;
+        }
+
+        public void setCantidad(BigDecimal cantidad) {
+            this.cantidad = cantidad;
+        }
+
+        public BigDecimal getTotal() {
+            return total;
+        }
+
+        public void setTotal(BigDecimal total) {
+            this.total = total;
+        }
+
+        public BigInteger getIva() {
+            return iva;
+        }
+
+        public void setIva(BigInteger iva) {
+            this.iva = iva;
+        }
+		//String productName;
+
+
+        public Tentidad getEntidad() {
+            return entidad;
+        }
+
+    
+        public void setEntidad(Tentidad entidad) {
+            this.entidad = entidad;
+        }
+		
+
+		
+		
+		
+	}
+} 
---
+++ sistemaProformasOnlineIGMv1/sistemaProformasOnlineIGMv1-web/src/main/java/gob/igm/ec/servicios/TitemServicio.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gob.igm.ec.servicios;
+
+import gob.igm.ec.Titem;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Local;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author PULE_DIEGO
+ */
+@Stateless
+@LocalBean
+public class TitemServicio {
+    @PersistenceContext(unitName = "gob.igm.ec_sistemaProformasOnlineIGMv1-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+  
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+  
+        public List<Titem> findAll(){
+        List<Titem> lista = new ArrayList<Titem>();
+        Query query = em.createQuery("SELECT t FROM Titem t WHERE t.lVentaOnline = 1");
+        lista.addAll(query.getResultList());
+          return lista; 
+     
+          
+       
+    }
+    
+    public Titem getDatos(int iditem)
+    {
+        Titem item = new Titem();
+        try{
+        Query query = em.createQuery("SELECT t FROM Titem t where t.idItem=?1");
+        query.setParameter(1, iditem);
+        item=(Titem)query.getSingleResult();
+        } catch (Exception e) {
+            
+            
+            
+            
+        }
+        return item;
+    }
+    
+    
+   
+}
