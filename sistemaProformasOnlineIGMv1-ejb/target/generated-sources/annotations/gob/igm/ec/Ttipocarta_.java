966db60c871dd967a2c42208caccce9e390404e5
jctoapanta <jc_toapanta@yahoo.com>
2018-07-05 16:23:46 -0500

    Aporte Juanka: Se agrega Bootstrap

--- sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Ttipocarta_.java
+++ sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Ttipocarta_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-04T15:33:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-05T09:15:05")
 @StaticMetamodel(Ttipocarta.class)
 public class Ttipocarta_ { 
 
