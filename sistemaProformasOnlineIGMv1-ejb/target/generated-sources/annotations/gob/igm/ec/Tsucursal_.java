244639fba1f375d21205e83977c87c0a8aca3674
jctoapanta <jc_toapanta@yahoo.com>
2018-07-06 13:24:13 -0500

    Aporte Juanka: Se modifica a pgina responsiva

--- sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tsucursal_.java
+++ sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tsucursal_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-05T09:15:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-06T13:22:57")
 @StaticMetamodel(Tsucursal.class)
 public class Tsucursal_ { 
 
