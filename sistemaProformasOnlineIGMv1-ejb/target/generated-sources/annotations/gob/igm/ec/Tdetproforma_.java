6c0fdb5faffd4f119a03e1cf28db1c82fb614284
PULE_DIEGO <PULE_DIEGO@TEC_DES16.igm.mil>
2018-07-06 13:59:06 -0500

    PDF terminado con las direcciones y calculos de recargas OK

--- sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tdetproforma_.java
+++ sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tdetproforma_.java
@@ -11,7 +11,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-06T13:22:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-06T13:56:57")
 @StaticMetamodel(Tdetproforma.class)
 public class Tdetproforma_ { 
 
