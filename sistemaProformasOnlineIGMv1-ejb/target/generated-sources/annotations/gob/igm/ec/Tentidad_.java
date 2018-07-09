40d97280f770054d47203b8e26fe827c686a862d
jctoapanta <jc_toapanta@yahoo.com>
2018-07-09 12:37:53 -0500

    Aporte Juanka: Se modifica pantalla de proformas a MODAL

--- sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tentidad_.java
+++ sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tentidad_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-06T14:10:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-09T11:04:37")
 @StaticMetamodel(Tentidad.class)
 public class Tentidad_ { 
 
