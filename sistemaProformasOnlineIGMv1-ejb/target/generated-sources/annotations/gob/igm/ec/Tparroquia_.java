4bbfb23ddb8fab8b1086dfd5e40928a2d91bc904
PULE_DIEGO <PULE_DIEGO@TEC_DES16.igm.mil>
2018-09-25 10:26:26 -0500

    Mejor el diseño de la pagina web barra de proceso

--- sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tparroquia_.java
+++ sistemaProformasOnlineIGMv1-ejb/target/generated-sources/annotations/gob/igm/ec/Tparroquia_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-29T12:59:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-19T14:24:43")
 @StaticMetamodel(Tparroquia.class)
 public class Tparroquia_ { 
 
